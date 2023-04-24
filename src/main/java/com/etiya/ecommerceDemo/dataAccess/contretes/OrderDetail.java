164a203bb3021d7c457c046e8ee75029d2b7a866
mustafa <mustafaciceknote8@gmail.com>
2023-04-24 19:16:01 +0300

    fix

--- src/main/java/com/etiya/ecommerceDemo/dataAccess/contretes/OrderDetail.java
+++
@@ -1,6 +0,0 @@
-package com.etiya.ecommerceDemo.dataAccess.contretes;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderDetail extends JpaRepository<OrderDetail, Long> {
-}
