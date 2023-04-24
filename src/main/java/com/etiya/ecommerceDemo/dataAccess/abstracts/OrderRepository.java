d7de3bbf620b93be9a078e4e60637f79a821417e
yucemetin <red.banana988@gmail.com>
2023-04-24 15:47:20 +0300

    .

--- src/main/java/com/etiya/ecommerceDemo/dataAccess/abstracts/OrderRepository.java
+++ src/main/java/com/etiya/ecommerceDemo/dataAccess/abstracts/OrderRepository.java
@@ -3,5 +3,5 @@ package com.etiya.ecommerceDemo.dataAccess.abstracts;
 import com.etiya.ecommerceDemo.entities.concrete.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OrderRepository extends JpaRepository<Order,Integer> {
+public interface OrderRepository extends JpaRepository<Order,Long> {
 }
