de18ff1488cfa37911a4cb1a68f1efc8c528985b
yucemetin <red.banana988@gmail.com>
2023-04-25 16:15:09 +0300

    category, installment and invoice repositories are updated

--- src/main/java/com/etiya/ecommerceDemo/dataAccess/abstracts/CategoryRepository.java
+++ src/main/java/com/etiya/ecommerceDemo/dataAccess/abstracts/CategoryRepository.java
@@ -2,6 +2,11 @@ package com.etiya.ecommerceDemo.dataAccess.abstracts;
 
 import com.etiya.ecommerceDemo.entities.concrete.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface CategoryRepository extends JpaRepository<Category,Integer> {
+    @Query(value = "SELECT c.* FROM categories c INNER JOIN products p on p.id = c.id", nativeQuery = true)
+    List<Category> getCategoriesByProductId();
 }
--- src/main/java/com/etiya/ecommerceDemo/dataAccess/abstracts/InstallmentRepository.java
+++ src/main/java/com/etiya/ecommerceDemo/dataAccess/abstracts/InstallmentRepository.java
@@ -2,6 +2,14 @@ package com.etiya.ecommerceDemo.dataAccess.abstracts;
 
 import com.etiya.ecommerceDemo.entities.concrete.Installment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface InstallmentRepository extends JpaRepository<Installment,Long> {
+    @Query(value = "SELECT ins.* FROM installments ins INNER JOIN payments p ON ins.payment_id = p.id", nativeQuery = true)
+    List<Installment> getInstallmentByPaymentId();
+
+    @Query(value = "SELECT ins.* FROM installments ins INNER JOIN orders o ON ins.order_id = o.id", nativeQuery = true)
+    List<Installment> getInstallmentByOrderId();
 }
