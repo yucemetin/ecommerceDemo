df68a7c7a3eb739f80dfeb2cad0a906101dc7c42
yucemetin <red.banana988@gmail.com>
2023-04-25 16:09:34 +0300

    address repository updated

--- src/main/java/com/etiya/ecommerceDemo/dataAccess/abstracts/AddressRepository.java
+++ src/main/java/com/etiya/ecommerceDemo/dataAccess/abstracts/AddressRepository.java
@@ -2,6 +2,11 @@ package com.etiya.ecommerceDemo.dataAccess.abstracts;
 
 import com.etiya.ecommerceDemo.entities.concrete.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface AddressRepository extends JpaRepository<Address,Long> {
+    @Query(value = "SELECT a.* FROM addresses a INNER JOIN users u ON u.id = a.user_id", nativeQuery = true)
+    List<Address> getAddressByUserId();
 }
