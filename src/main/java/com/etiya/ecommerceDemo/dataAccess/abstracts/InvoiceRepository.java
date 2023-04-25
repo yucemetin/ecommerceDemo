3af2ba04945a44405a434d8353bc35388f25f613
yucemetin <red.banana988@gmail.com>
2023-04-25 16:15:09 +0300

    category, installment and invoice repositories are updated

--- src/main/java/com/etiya/ecommerceDemo/dataAccess/abstracts/InvoiceRepository.java
+++ src/main/java/com/etiya/ecommerceDemo/dataAccess/abstracts/InvoiceRepository.java
@@ -2,6 +2,11 @@ package com.etiya.ecommerceDemo.dataAccess.abstracts;
 
 import com.etiya.ecommerceDemo.entities.concrete.Invoice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface InvoiceRepository extends JpaRepository<Invoice,Long> {
+    @Query(value = "SELECT inv.* FROM invoices inv INNER JOIN orders o ON inv.order_id = o.id", nativeQuery = true)
+    List<Invoice> getInvoiceByOrderId();
 }
