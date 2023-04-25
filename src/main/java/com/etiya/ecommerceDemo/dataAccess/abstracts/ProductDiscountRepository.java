91a65a91c98825d9ab809ca68ee894d90184e9ac
mustafa <mustafaciceknote8@gmail.com>
2023-04-25 13:04:47 +0300

    jpql add

--- src/main/java/com/etiya/ecommerceDemo/dataAccess/abstracts/ProductDiscountRepository.java
+++ src/main/java/com/etiya/ecommerceDemo/dataAccess/abstracts/ProductDiscountRepository.java
@@ -1,7 +1,20 @@
 package com.etiya.ecommerceDemo.dataAccess.abstracts;
 
+import com.etiya.ecommerceDemo.entities.concrete.Product;
 import com.etiya.ecommerceDemo.entities.concrete.ProductDiscount;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ProductDiscountRepository extends JpaRepository<ProductDiscount,Integer> {
+    @Query(value = "SELECT * FROM public.product_discount")
+    List<Product> getProductDiscount();
+    @Query(value = "SELECT * FROM public.product_discount pd where pd.discount_amount = (select MAX(pd2.discount_amount) FROM public.product_discount pd2)")
+    List<ProductDiscount> getMaxProductDiscount();
+
+
+
+
 }
