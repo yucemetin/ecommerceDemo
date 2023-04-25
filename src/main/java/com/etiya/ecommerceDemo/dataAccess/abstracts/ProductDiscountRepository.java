b1346855754c27c7c387e28fb1bf140dd4d759c4
yucemetin <red.banana988@gmail.com>
2023-04-25 15:57:17 +0300

    prodcut_discount repository updated

--- src/main/java/com/etiya/ecommerceDemo/dataAccess/abstracts/ProductDiscountRepository.java
+++ src/main/java/com/etiya/ecommerceDemo/dataAccess/abstracts/ProductDiscountRepository.java
@@ -9,9 +9,9 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface ProductDiscountRepository extends JpaRepository<ProductDiscount,Integer> {
-    @Query(value = "SELECT * FROM public.product_discount")
+    @Query(value = "SELECT * FROM product_discount", nativeQuery = true)
     List<Product> getProductDiscount();
-    @Query(value = "SELECT * FROM public.product_discount pd where pd.discount_amount = (select MAX(pd2.discount_amount) FROM public.product_discount pd2)")
+    @Query(value = "SELECT * FROM product_discount pd where pd.discount_amount = (select MAX(pd2.discount_amount) FROM product_discount pd2)",nativeQuery = true)
     List<ProductDiscount> getMaxProductDiscount();
 
 }
