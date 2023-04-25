f27ec04176839873df24a0a964dfb6596bb3c580
yucemetin <red.banana988@gmail.com>
2023-04-25 12:34:10 +0300

    jpql join added

--- src/main/java/com/etiya/ecommerceDemo/dataAccess/abstracts/ReturnRepository.java
+++ src/main/java/com/etiya/ecommerceDemo/dataAccess/abstracts/ReturnRepository.java
@@ -1,7 +1,14 @@
 package com.etiya.ecommerceDemo.dataAccess.abstracts;
 
+import com.etiya.ecommerceDemo.entities.concrete.Order;
 import com.etiya.ecommerceDemo.entities.concrete.Return;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ReturnRepository extends JpaRepository<Return,Integer> {
+
+    @Query(value = "SELECT r.* FROM order o INNER JOIN returns r ON o.order_id = r.order_id", nativeQuery = true)
+    List<Return> getOrderByReturns();
 }
