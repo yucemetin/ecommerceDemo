20a58910fbf316112fa0820a21d19ce62554e38c
mustafa <mustafaciceknote8@gmail.com>
2023-04-26 04:07:44 +0300

    jpql ProductSupplierDao

--- src/main/java/com/etiya/ecommerceDemo/repositories/abstracts/ProductSupplierDao.java
+++ src/main/java/com/etiya/ecommerceDemo/repositories/abstracts/ProductSupplierDao.java
@@ -2,6 +2,12 @@ package com.etiya.ecommerceDemo.repositories.abstracts;
 
 import com.etiya.ecommerceDemo.entities.concretes.ProductSupplier;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ProductSupplierDao extends JpaRepository<ProductSupplier,Long> {
+    @Query(value = "SELECT ps.* FROM product_suppliers ps INNER JOIN suppliers s ON s.id = ps.supplier_id WHERE s.country = :country", nativeQuery = true)
+    List<ProductSupplier> getProductSuppliersByCountry();
+
 }
