8ed9e72fb22b06a62cf011916044e8e630db235b
mustafa <mustafaciceknote8@gmail.com>
2023-04-26 04:03:46 +0300

    jpql ProductReviewDao

--- src/main/java/com/etiya/ecommerceDemo/repositories/abstracts/ProductReviewDao.java
+++ src/main/java/com/etiya/ecommerceDemo/repositories/abstracts/ProductReviewDao.java
@@ -2,6 +2,12 @@ package com.etiya.ecommerceDemo.repositories.abstracts;
 
 import com.etiya.ecommerceDemo.entities.concretes.ProductReview;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ProductReviewDao extends JpaRepository<ProductReview,Long> {
+    @Query(value = "SELECT pr.* FROM product_reviews pr INNER JOIN products p ON p.id = pr.product_id WHERE p.id = :productId", nativeQuery = true)
+    List<ProductReview> getProductReviewsByProductId();
+
 }
